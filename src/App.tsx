8aa411bc0b6b7bc704c128c69d83fb9345c16abc
DARSHAN VAGHELA <vdarshan613@gmail.com>
2024-07-19 23:42:37 +0530

    Changes

--- src/App.tsx
+++ src/App.tsx
@@ -24,33 +24,35 @@ const App = () => {
   return (
     <>
       <div className="p-4 max-w-[1100px] mx-auto my-0">
-        <div className="flex mb-4 justify-between items-center">
+        <div className="flex flex-col md:flex-row mb-4 justify-between items-start">
           <input
             type="text"
-            className="rounded-md h-12 outline-none shadow-md p-4 w-[200px] md:w-[300px]"
+            className="rounded-md h-12 outline-none shadow-md p-4 w-full md:w-[300px]"
             placeholder="Search..."
             onChange={(e) => debouncedSearch(e.target.value)}
           />
-          <div className="flex gap-3">
+          <div className="flex flex-col lg:flex-row gap-4 mt-4 md:mt-0 items-start">
             <MultiDatePicker
               selectedDateRange={selectedDateRange}
               setSelectedDateRange={setSelectedDateRange}
             />
-            <select
-              id="sort"
-              value={sort}
-              onChange={(e) => setSort(e.target.value)}
-              className="border h-10 rounded-md outline-none"
-            >
-              <option value="newest">Newest</option>
-              <option value="oldest">Oldest</option>
-            </select>
-            <button
-              className="bg-blue-500 text-white font-medium hover:bg-blue-700 transition-all hover:font-semibold ease-in-out duration-200 px-4 py-2 rounded mb-4"
-              onClick={() => setIsModalOpen(true)}
-            >
-              Add Product
-            </button>
+            <div className="flex items-start gap-4">
+              <select
+                id="sort"
+                value={sort}
+                onChange={(e) => setSort(e.target.value)}
+                className="border h-10 rounded-md outline-none"
+              >
+                <option value="newest">Newest</option>
+                <option value="oldest">Oldest</option>
+              </select>
+              <button
+                className="bg-blue-500 text-white font-medium hover:bg-blue-700 transition-all hover:font-semibold ease-in-out duration-200 px-4 py-2 rounded mb-4"
+                onClick={() => setIsModalOpen(true)}
+              >
+                Add Product
+              </button>
+            </div>
           </div>
         </div>
         <ProductListing
